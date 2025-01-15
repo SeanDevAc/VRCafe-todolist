df4ddbd6121d9133b0384af756342b1bd54d6773
SeanDevAc <seanwakker.website@gmail.com>
2025-01-15 11:05:48 +0100

    react setup + 2 components
    - React setup
    - Navbar added
    - Dashboard added
    - Basic routing

---
+++ vrctodoapp/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css';
+import NavBar from '../components/NavBar';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <NavBar />
+            <p>Hello World</p>
+        </div>
+    );
+};
+
+export default Dashboard;
