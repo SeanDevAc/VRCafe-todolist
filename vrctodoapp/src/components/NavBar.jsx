9367563c9de37747a75a61100a5f3c54bf67aa64
SeanDevAc <seanwakker.website@gmail.com>
2025-01-15 11:55:01 +0100

    tweaks + fallbackpage
    - 404 page added

--- vrctodoapp/src/components/NavBar.jsx
+++ vrctodoapp/src/components/NavBar.jsx
@@ -5,10 +5,11 @@ import logo from '../assets/img/vrcafe_logo.webp';
 const NavBar = () => {
     return (
         <div>
-            <div className="navBarWrapper"></div>
+            <div className="navBarBackgroundTop"></div>
             <a href="https://www.vrcafehaarlem.nl/">
                 <img src={logo} alt="VR Cafe Logo" className="logo" />
             </a>
+            <div className="navBarBackgroundBottom"></div>
         </div>
     );
 };
