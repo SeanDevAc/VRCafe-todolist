79a95201d217f15070ddfcdfac4724dd69b0c82d
SeanDevAc <seanwakker.website@gmail.com>
2025-01-15 11:17:47 +0100

    Advanced routing
    - Advanced routing added

---
+++ vrctodoapp/src/pages/FallBackPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css';
+import NavBar from '../components/NavBar';
+
+const FallBackPage = () => {
+    return (
+        <div>
+            <NavBar />
+            <p>Hello World</p>
+        </div>
+    );
+};
+
+export default FallBackPage;
